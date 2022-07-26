8a9e01fb1068f8c07a3899677e8ee1c7a3e27082
Yuriy Bevov <jury19y@mail.ru>
2022-07-26 16:26:22 +0300

    icon

--- build/assets/bundle.js
+++ build/assets/bundle.js
@@ -9573,85 +9573,12 @@ animateHeroContent();
 
 
 var textIcons = document.querySelectorAll('.text-svg');
-/*textIcons.forEach((icon,i) => {
-  gsap.set(icon, { autoAlpha: 1 });
-  let textPath = icon.querySelector('.circle');
-  const timeline = gsap.timeline({ repeat: -1, paused: true });
-
-  timeline.to(textPath, {
-    duration: 10,
-    ease: 'linear',
-    rotate: '360deg',
-    transformOrigin: "center center",
-  });
-
-  icon.addEventListener('mouseenter', () => {
-    //timeline.progress(0).play();
-    timeline.resume();
-  });
-
-  icon.addEventListener('mouseleave', () => {
-    //timeline.progress(1).pause();
-    timeline.pause();
-  });
-}) */
-
-/*textIcons.forEach((icon,i) => {
-  gsap.set(icon, { autoAlpha: 1 });
-  let textPath = icon.querySelector('.circle');
-  const timeline = gsap.timeline({ paused: true });
-  const timeline2 = gsap.timeline({ paused: true });
-
-  //gets total progress
-  var progress = timeline.totalProgress();
-  let debounce = false;
-
-  icon.addEventListener('mouseenter', () => {
-    timeline2.progress(1).pause();
-
-    if(debounce === false) {
-      console.log(debounce)
-      debounce = true;
-      timeline.to(textPath, {
-        duration: 1,
-        ease: 'linear',
-        rotate: '-45deg',
-        transformOrigin: "center center",
-      });
-      console.log(progress);
-      timeline.progress(0).play();
-
-      setTimeout(() => {
-        debounce = false;
-      }, 1000);
-    }
-  });
-
-  icon.addEventListener('mouseleave', () => {
-    timeline.progress(1).pause();
-    if(debounce === false) {
-      console.log(debounce)
-      debounce = true;
-      timeline2.to(textPath, {
-        duration: 1,
-        ease: 'linear',
-        rotate: '0',
-        transformOrigin: "center center",
-      });
-
-      timeline2.progress(0).play();
-      setTimeout(() => {
-        debounce = false;
-      }, 1000);
-    }
-  });
-})*/
-
 textIcons.forEach(function (icon) {
   gsap__WEBPACK_IMPORTED_MODULE_4__.gsap.set(icon, {
     autoAlpha: 1
   });
   var textPath = icon.querySelector('.circle');
+  var ic = icon.querySelector('.icon');
   icon.addEventListener('mouseenter', function () {
     gsap__WEBPACK_IMPORTED_MODULE_4__.gsap.to(textPath, {
       duration: 2,
@@ -9660,12 +9587,26 @@ textIcons.forEach(function (icon) {
       rotate: '360deg',
       transformOrigin: "center center"
     });
+    gsap__WEBPACK_IMPORTED_MODULE_4__.gsap.to(ic, {
+      duration: 2,
+      delay: 0.2,
+      ease: 'back',
+      rotate: '-360deg',
+      transformOrigin: "center center"
+    });
   });
   icon.addEventListener('mouseleave', function () {
     gsap__WEBPACK_IMPORTED_MODULE_4__.gsap.to(textPath, {
       duration: 3,
       delay: 0.2,
       ease: 'elastic',
+      rotate: '0',
+      transformOrigin: "center center"
+    });
+    gsap__WEBPACK_IMPORTED_MODULE_4__.gsap.to(ic, {
+      duration: 2,
+      delay: 0.2,
+      ease: 'back',
       rotate: '0',
       transformOrigin: "center center"
     });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,83 +18,10 @@ import {gsap} from 'gsap';
 
 const textIcons = document.querySelectorAll('.text-svg');
 
-/*textIcons.forEach((icon,i) => {
-  gsap.set(icon, { autoAlpha: 1 });
-  let textPath = icon.querySelector('.circle');
-  const timeline = gsap.timeline({ repeat: -1, paused: true });
-
-  timeline.to(textPath, {
-    duration: 10,
-    ease: 'linear',
-    rotate: '360deg',
-    transformOrigin: "center center",
-  });
-
-  icon.addEventListener('mouseenter', () => {
-    //timeline.progress(0).play();
-    timeline.resume();
-  });
-
-  icon.addEventListener('mouseleave', () => {
-    //timeline.progress(1).pause();
-    timeline.pause();
-  });
-}) */
-
-/*textIcons.forEach((icon,i) => {
-  gsap.set(icon, { autoAlpha: 1 });
-  let textPath = icon.querySelector('.circle');
-  const timeline = gsap.timeline({ paused: true });
-  const timeline2 = gsap.timeline({ paused: true });
-
-  //gets total progress
-  var progress = timeline.totalProgress();
-  let debounce = false;
-
-  icon.addEventListener('mouseenter', () => {
-    timeline2.progress(1).pause();
-
-    if(debounce === false) {
-      console.log(debounce)
-      debounce = true;
-      timeline.to(textPath, {
-        duration: 1,
-        ease: 'linear',
-        rotate: '-45deg',
-        transformOrigin: "center center",
-      });
-      console.log(progress);
-      timeline.progress(0).play();
-
-      setTimeout(() => {
-        debounce = false;
-      }, 1000);
-    }
-  });
-
-  icon.addEventListener('mouseleave', () => {
-    timeline.progress(1).pause();
-    if(debounce === false) {
-      console.log(debounce)
-      debounce = true;
-      timeline2.to(textPath, {
-        duration: 1,
-        ease: 'linear',
-        rotate: '0',
-        transformOrigin: "center center",
-      });
-
-      timeline2.progress(0).play();
-      setTimeout(() => {
-        debounce = false;
-      }, 1000);
-    }
-  });
-})*/
-
 textIcons.forEach(icon => {
   gsap.set(icon, { autoAlpha: 1 });
   let textPath = icon.querySelector('.circle');
+  let ic = icon.querySelector('.icon');
 
   icon.addEventListener('mouseenter', () => {
     gsap.to(textPath, {
@@ -104,6 +31,13 @@ textIcons.forEach(icon => {
       rotate: '360deg',
       transformOrigin: "center center",
     })
+    gsap.to(ic, {
+      duration: 2,
+      delay: 0.2,
+      ease: 'back',
+      rotate: '-360deg',
+      transformOrigin: "center center",
+    })
   })
 
   icon.addEventListener('mouseleave', () => {
@@ -111,6 +45,13 @@ textIcons.forEach(icon => {
       duration: 3,
       delay: 0.2,
       ease: 'elastic',
+      rotate: '0',
+      transformOrigin: "center center",
+    })
+    gsap.to(ic, {
+      duration: 2,
+      delay: 0.2,
+      ease: 'back',
       rotate: '0',
       transformOrigin: "center center",
     })
